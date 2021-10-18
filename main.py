e3e83dafd4eddfa28eea1e27ba31f3ba296bfd15
Poehee <bartwww@gmail.com>
2021-10-18 22:50:31 +0200

    Playing around with block, blit and flip

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-#try:
-#    from pygame import *
-#except ImportError:
-#    raise ImportError('<any message you want here>')
 import pygame
 
 # Import pygame.locals for easier access to key coordinates
@@ -20,12 +16,13 @@ pygame.init()
 
 clock = pygame.time.Clock()
 
-FPS = 30 # How many times the screen will update per second
+FPS = 60 # How many times the screen will update per second
 
 screen_width = 1280 # How wide the window will be
 screen_height = 1024 # how high the window will be
 
 screen = pygame.display.set_mode([screen_width, screen_height]) # creates the screen
+surface_pos = [590,462]
 
 # Variable to keep the main loop running
 running = True
@@ -42,8 +39,15 @@ while running:
             running = False
 
     # Fill the screen with white
-    screen.fill((255, 255, 255))
+    screen.fill([255, 255, 255])
+    sf = pygame.Surface([100,100])
+    sf.fill([50,50,50])
+    rect = sf.get_rect()
 
+    screen.blit(sf, surface_pos)
+    surface_pos[0] += 1
+    surface_pos[1] += 1
+    
     pygame.display.flip()
 
 pygame.quit()
