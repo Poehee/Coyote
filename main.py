8f3a3cab04d79c9c38a0e9d5540c69ce3031a5d3
Poehee <bartwww@gmail.com>
2021-10-18 22:09:15 +0200

    main.py sees the light

---
+++ main.py
@@ -0,0 +1,49 @@
+#try:
+#    from pygame import *
+#except ImportError:
+#    raise ImportError('<any message you want here>')
+import pygame
+
+# Import pygame.locals for easier access to key coordinates
+# Updated to conform to flake8 and black standards
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+pygame.init()
+
+clock = pygame.time.Clock()
+
+FPS = 30 # How many times the screen will update per second
+
+screen_width = 1280 # How wide the window will be
+screen_height = 1024 # how high the window will be
+
+screen = pygame.display.set_mode([screen_width, screen_height]) # creates the screen
+
+# Variable to keep the main loop running
+running = True
+while running:
+    for event in pygame.event.get():
+        # Did the user hit a key?
+        if event.type == KEYDOWN:
+            # Was it the Escape key? If so, stop the loop.
+            if event.key == K_ESCAPE:
+                running = False
+
+        # Did the user click the window close button? If so, stop the loop.
+        elif event.type == QUIT:
+            running = False
+
+    # Fill the screen with white
+    screen.fill((255, 255, 255))
+
+    pygame.display.flip()
+
+pygame.quit()
